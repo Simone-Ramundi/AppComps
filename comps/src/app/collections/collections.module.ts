2728fde6ed5e19965bf7e04f57111454db9826b2
Simone <simoneramundi@outlook.com>
2023-10-10 13:18:17 +0200

    Module exercise solution

--- comps/src/app/collections/collections.module.ts
+++ comps/src/app/collections/collections.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
-
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CollectionsHomeComponent
+  ],
+  exports: [
+    CollectionsHomeComponent
+  ],
   imports: [
     CommonModule,
     CollectionsRoutingModule
