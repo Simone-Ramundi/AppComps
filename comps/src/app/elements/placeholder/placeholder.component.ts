05ff36c18574e99ed39f53e79ef45db2e3c26928
Simone <simoneramundi@outlook.com>
2023-10-11 12:56:35 +0200

    Added directive Times

---
+++ comps/src/app/elements/placeholder/placeholder.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit,Input } from '@angular/core';
+import * as trace_events from "trace_events";
+
+@Component({
+  selector: 'app-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.css']
+})
+export class PlaceholderComponent implements OnInit {
+  @Input() header = true;
+  @Input() lines = 3;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
