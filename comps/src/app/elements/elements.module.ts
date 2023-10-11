11c164ec117f828193960d553d054662cca555cc
Simone <simoneramundi@outlook.com>
2023-10-11 14:51:39 +0200

    Implementing a title divider

--- comps/src/app/elements/elements.module.ts
+++ comps/src/app/elements/elements.module.ts
@@ -5,7 +5,7 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
-
+import { SharedModule } from "../shared/shared.module";
 
 
 @NgModule({
@@ -17,7 +17,8 @@ import { TimesDirective } from './times.directive';
     exports: [],
     imports: [
         CommonModule,
-        ElementsRoutingModule
+        ElementsRoutingModule,
+        SharedModule
     ]
 })
 export class ElementsModule { }
