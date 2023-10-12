e1797c407bc815a634ca811bdc7120d0dd875f71
Simone <simoneramundi@outlook.com>
2023-10-12 15:22:57 +0200

    Update

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -7,14 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    { name: 'Simone', age: '24', job:'Developer'},
-    { name: 'Mattia', age: '30', job:'Developer'},
-    { name: 'Carlo', age: '25', job:'Developer'}
+    { name: 'Simone', age: '24', job:'Developer', employed: true },
+    { name: 'Mattia', age: '30', job:'Developer', employed: true},
+    { name: 'Carlo', age: '25', job:'Developer', employed: true}
   ];
   headers = [
     { key: 'name', label: 'Name'},
     { key: 'age', label: 'Age'},
-    { key: 'job', label: 'Job'}
+    { key: 'job', label: 'Job'},
+    { key: 'employed', label: 'Has a job?'}
   ]
   constructor() { }
 
