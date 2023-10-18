c397457ad9525a92bf4a10ac886198515edcbf1b
Simone Ramundi <simone.ramundi@4science.com>
2023-10-18 11:21:57 +0200

    The Mods Module

--- comps/src/app/app-routing.module.ts
+++ comps/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path:'elements', loadChildren: () => import('./elements/elements.module').then((m) => m.ElementsModule)},
   { path:'collections', loadChildren:() => import('./collections/collections.module').then((m)=> m.CollectionsModule)},
   { path:'views', loadChildren:() => import('./views/views.module').then((m)=> m.ViewsModule)},
+  { path:'mods', loadChildren:() => import('./mods/mods.module').then((m)=> m.ModsModule)},
   { path: '', component: HomeComponent},
   { path:'**', component: NotFoundComponent},
 ];
