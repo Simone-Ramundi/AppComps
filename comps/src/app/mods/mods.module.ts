8fd6b4a7c88d08900f4d9d6b176d1e7b295dc43a
Simone Ramundi <simone.ramundi@4science.com>
2023-10-18 11:30:13 +0200

    Modal Window Setup

--- comps/src/app/mods/mods.module.ts
+++ comps/src/app/mods/mods.module.ts
@@ -4,16 +4,17 @@ import { CommonModule } from '@angular/common';
 import { ModsRoutingModule } from './mods-routing.module';
 import { ModsHomeComponent } from './mods-home/mods-home.component';
 import { ModalComponent } from './modal/modal.component';
-
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
     ModsHomeComponent,
-    ModalComponent
+    ModalComponent,
   ],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
